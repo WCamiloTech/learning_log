f7ca4d83f72790950d43cf0c19685bf1a875c83f
Williames Camilo <williamescw@gmail.com>
2026-04-23 03:59:36 -0300

    Criando um template base

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -2,6 +2,9 @@
 from . import views
 from django.urls import path
 
+# Adicione esta linha:
+app_name = 'learning_logs'
+
 urlpatterns = [
     # Página inicial
     path("", views.index, name="index"),
