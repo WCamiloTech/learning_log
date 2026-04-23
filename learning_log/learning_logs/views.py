6caaf1d6871e45e060fe64727a69f62179a40e06
Williames Camilo <williamescw@gmail.com>
2026-04-23 15:00:52 -0300

    View de um assunto

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -6,10 +6,20 @@ def index(request):
     """A página inicial de Learning Log"""
     return render(request, 'learning_logs/index.html')
 
-def topics(request):
-    """Mostra todos os assuntos"""
+def topics(request, topic_id):
+    """Mostra todos os assuntos e todas as suas entradas"""
     topics = Topic.objects.order_by('date_added')
     context = {
         'topics': topics,
     }
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {
+        'topic': topic,
+        'entries': entries,
+    }
+    return render(request, 'learning_logs/topic.html', context)
